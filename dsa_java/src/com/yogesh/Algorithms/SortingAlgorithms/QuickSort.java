35209db38e4b84103173e8d74cde2cb142845f31
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-06-23 09:47:23 +0530

    Quick Sort Partition Algoorithms

--- dsa_java/src/com/yogesh/Algorithms/SortingAlgorithms/QuickSort.java
+++ dsa_java/src/com/yogesh/Algorithms/SortingAlgorithms/QuickSort.java
@@ -10,22 +10,64 @@ public class QuickSort {
     private void sort(int[] arr, int start, int end){
         if (start >= end) return;
         //Partition array
-        int boundary = partition(arr, start, end);
+        int boundary = lomutoPartition(arr, start, end);
         //Sort left subarray
-        sort(arr, start, boundary - 1);
+        sort(arr, start, boundary - 1); //pass only boundary instead of boundary-1 for Hoare's Partition
         //Sort right subarray
         sort(arr, boundary + 1, end);
     }
 
-    private int partition(int[] arr, int start, int end){
+    //Time-O(n), Aux Space - O(1)
+    //Select Last element as pivot
+    private int lomutoPartition(int[] arr, int start, int end){
         int pivot = arr[end];
         int boundary = start - 1;
 
-        for (int i = start; i <= end; i++){
+        for (int i = start; i <= end; i++)
             if (arr[i] <= pivot)
                 swap(arr, i, ++boundary);
-        }
+
         return boundary;
+    }
+
+    //Time-O(n), Aux Space - O(1). Hoare's partition works much better than Lomuto's Partition
+    //Select First element as pivot
+    private int hoaresPartition(int[] arr, int start, int end){
+        int pivot = arr[start];
+        int i = start - 1;
+        int j = end + 1;
+        while (true){
+            do {
+                i++;
+            }while (arr[i] < pivot);
+
+            do {
+                j--;
+            }while (arr[j] > pivot);
+
+            if (i >= j) return j;
+            swap(arr, i ,j);
+        }
+    }
+
+    //Time-O(n), Aux Space - O(n)
+    private void naivePartition(int[] arr, int start, int end, int pivot){
+        int[] temp = new int[end - start + 1];
+        int index = 0;
+
+        //copying elements smaller than or equal to pivot to temp
+        for (int i = start; i <= end; i++)
+            if (arr[i] <= arr[pivot])
+                temp[index++] = arr[i];
+
+        //copying elements larger than pivot to temp
+        for (int i = start; i <= end; i++)
+            if (arr[i] > arr[pivot])
+                temp[index++] = arr[i];
+
+        //copying elements back to original arr
+        for (int i = start; i <= end; i++)
+            arr[i] = temp[i - start];
     }
 
     private void swap(int[] arr, int index1, int index2){
