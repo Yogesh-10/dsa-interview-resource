d36e978ee56e9f7cefaed10973f6ca59d9cde875
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-07-17 23:55:19 +0530

    sliding window

--- dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
+++ dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
@@ -1456,7 +1456,31 @@ public class ArrayProblems {
         return maxLength;
     }
 
+    //Longest Subarray with Ones after Replacement
+    //Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
+    //Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2 //Output: 6 //Explanation: Replace the '0' at index 5 and 8 to have the longest contiguous subarray of 1s having length 6.
+    //Input: Array=[0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3 //Output: 9 //Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguous subarray of 1s having length 9.
+    public static int replacingOnes(int[] arr, int k){
+        //Time Complexity - O(N), Space Complexity - O(1)
+        int windowStart = 0; int maxLength = 0; int maxOnesCount = 0;
 
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+            if (arr[windowEnd] == 1)
+                maxOnesCount++;
+
+            // current window size is from windowStart to windowEnd, overall we have a maximum of 1s repeating a maximum of 'maxOnesCount' times, this means that we can have a window with
+            // 'maxOnesCount' 1s and the remaining are 0s which should replace with 1s. now, if the remaining 0s are more than 'k', it is the time to shrink the window as we are not allowed to replace more than 'k' Os
+            if (windowEnd - windowStart + 1 - maxOnesCount > k){
+                if (arr[windowStart] == 1)
+                    maxOnesCount--;
+
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
 
 }
 
