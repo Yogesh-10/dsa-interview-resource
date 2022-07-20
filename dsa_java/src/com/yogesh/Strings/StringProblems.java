656fcecc845801041d53456d7990573878510776
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-07-20 23:13:46 +0530

    String Problems

--- dsa_java/src/com/yogesh/Strings/StringProblems.java
+++ dsa_java/src/com/yogesh/Strings/StringProblems.java
@@ -1,4 +1,19 @@
 package com.yogesh.Strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringProblems {
+    public static char firstNonRepeatingCharacter(String str){
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for (char ch : chars)
+            charFrequencyMap.put(ch, charFrequencyMap.getOrDefault(ch, 0) + 1);
+
+        for (char ch : chars)
+            if (charFrequencyMap.get(ch) == 1)
+                return ch;
+
+        return Character.MIN_VALUE;
+    }
 }
