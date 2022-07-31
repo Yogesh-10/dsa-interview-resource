7a6597e4996a19d8d96be4828355e188ac8a1719
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-07-31 23:50:29 +0530

    matching pattern-rabinkarp

--- dsa_java/src/com/yogesh/Strings/StringProblems.java
+++ dsa_java/src/com/yogesh/Strings/StringProblems.java
@@ -227,4 +227,59 @@ public class StringProblems {
                 i = (i + j);
         }
     }
+
+    public static void RabinKarpAlgorithm(String str, String pattern){
+        int M = pattern.length();
+        int N = str.length();
+        int i, j;
+        int p = 0; // hash value for pattern
+        int t = 0; // hash value for txt
+        int h = 1;
+        int d = 256;
+        int q=101;
+        // The value of h would be "pow(d, M-1)%q"
+        for (i = 0; i < M-1; i++)
+            h = (h*d)%q;
+
+        // Calculate the hash value of pattern and first
+        // window of text
+        for (i = 0; i < M; i++) {
+            p = (d*p + pattern.charAt(i))%q;
+            t = (d*t + str.charAt(i))%q;
+        }
+
+        // Slide the pattern over text one by one
+        for (i = 0; i <= N - M; i++)
+        {
+
+            // Check the hash values of current window of text
+            // and pattern. If the hash values match then only
+            // check for characters one by one
+            if ( p == t )
+            {
+                /* Check for characters one by one */
+                for (j = 0; j < M; j++)
+                {
+                    if (str.charAt(i+j) != pattern.charAt(j))
+                        break;
+                }
+
+                // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
+                if (j == M)
+                    System.out.println("Pattern found at index " + i);
+            }
+
+            // Calculate hash value for next window of text: Remove
+            // leading digit, add trailing digit
+            if ( i < N-M )
+            {
+                t = (d*(t - str.charAt(i)*h) + str.charAt(i+M))%q;
+
+                // We might get negative value of t, converting it
+                // to positive
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+    }
 }
