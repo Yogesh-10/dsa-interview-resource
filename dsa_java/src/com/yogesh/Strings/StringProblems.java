d0fa99ccdc808ed75063fefb7b88f1d257e082b7
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-08-15 23:42:39 +0530

    string compare problems

--- dsa_java/src/com/yogesh/Strings/StringProblems.java
+++ dsa_java/src/com/yogesh/Strings/StringProblems.java
@@ -807,5 +807,45 @@ public class StringProblems {
         return resultIndices;
     }
 
-    //Comparing Strings containing Backspaces
+    //Comparing Strings containing Backspaces - Given two strings containing backspaces (identified by the character ‘#’), check if the two strings are equal.
+    //Input: str1="xy#z", str2="xzz#" Output: true Explanation: After applying backspaces the strings become "xz" and "xz" respectively.
+    //Input: str1="xy#z", str2="xyz#" Output: false Explanation: After applying backspaces the strings become "xz" and "xy" respectively.
+    //Input: str1="xp#", str2="xyz##" Output: true Explanation: After applying backspaces the strings become "x" and "x" respectively. In "xyz##", the first '#' removes the character 'z' and the second '#' removes the character 'y'.
+    //Input: str1="xywrrmp", str2="xywrrmu#p" Output: true Explanation: After applying backspaces the strings become "xywrrmp" and "xywrrmp" respectively.
+    public static boolean compareStringsContainingBackspaces(String str1, String str2) {
+        //The time complexity of the above algorithm will be O(M+N) where ‘M’ and ‘N’ are the lengths of the two input strings respectively.
+        //use two pointer approach to compare the strings
+        int index1 = str1.length() - 1, index2 = str2.length() - 1;
+        while (index1 >= 0 || index2 >= 0) {
+            int i1 = getNextValidCharIndex(str1, index1);
+            int i2 = getNextValidCharIndex(str2, index2);
+
+            if (i1 < 0 && i2 < 0) // reached the end of both the strings
+                return true;
+
+            if (i1 < 0 || i2 < 0) // reached the end of one of the strings
+                return false;
+
+            if (str1.charAt(i1) != str2.charAt(i2)) // check if the characters are equal
+                return false;
+
+            index1 = i1 - 1;
+            index2 = i2 - 1;
+        }
+        return true;
+    }
+    private static int getNextValidCharIndex(String str, int index) {
+        int backspaceCount = 0;
+        while (index >= 0) {
+            if (str.charAt(index) == '#') // found a backspace character
+                backspaceCount++;
+            else if (backspaceCount > 0) // a non-backspace character
+                backspaceCount--;
+            else
+                break;
+
+            index--; // skip a backspace or a valid character
+        }
+        return index;
+    }
 }
