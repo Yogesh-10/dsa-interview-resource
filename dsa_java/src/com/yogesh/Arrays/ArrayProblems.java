b52748993c2b311ace9d5932daaec15cd61d2b4a
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-10-10 22:20:05 +0530

    two pointers

--- dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
+++ dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
@@ -2101,7 +2101,7 @@ public class ArrayProblems {
         return res;
     }
 
-    //Squaring a Sorted Array
+    //62.Squaring a Sorted Array
     //Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
     //Input: [-2, -1, 0, 2, 3], Output: [0, 1, 4, 4, 9]  //Input: [-3, -1, 0, 1, 2] Output: [0 1 1 4 9]
     public static int[] makeSquaresInSortedArr(int[] arr) {
