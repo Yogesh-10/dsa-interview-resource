5eac34df716b5725027da7c708f2a7e22387df91
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-01-16 17:06:05 +0530

    ll problems

--- dsa_java/src/com/yogesh/Linkedlist/LinkedListProblems.java
+++ dsa_java/src/com/yogesh/Linkedlist/LinkedListProblems.java
@@ -3,9 +3,10 @@ package com.yogesh.Linkedlist;
 import com.yogesh.Linkedlist.LinkedList.Node;
 
 public class LinkedListProblems {
+    //1. Search a value in linkedList
     public static int searchInLinkedList(Node head, int x){
         //TC-O(n), SC-O(1)
-//        Node curr = head;
+//      Node curr = head;
         int position = 1;
         while (head != null){
             if (head.value == x)
@@ -28,5 +29,21 @@ public class LinkedListProblems {
             else return (res + 1);
         }
  */
+    }
+
+    //2. Reverse a Linked List
+    public static Node reverse(Node head){
+        //TC-O(n), SC-O(1)
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
     }
 }
