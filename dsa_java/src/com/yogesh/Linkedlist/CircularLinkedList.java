91d1e00d73dc284ff3dbb829f19107cccff6d567
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-01-20 19:03:03 +0530

    add and delete CLL

--- dsa_java/src/com/yogesh/Linkedlist/CircularLinkedList.java
+++ dsa_java/src/com/yogesh/Linkedlist/CircularLinkedList.java
@@ -16,7 +16,7 @@ public class CircularLinkedList {
     public void addFirst(int value){
         Node newNode = new Node(value);
 
-        if (head == null) {
+        if (isEmpty()) {
             head = tail = newNode;
             newNode.next = head;
             return;
@@ -35,5 +35,69 @@ public class CircularLinkedList {
         curr.next = newNode;
         head = newNode;
  */
+    }
+
+    public void addLast(int value){
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            head = tail = newNode;
+            head.next = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.next = head;
+        tail = newNode;
+
+        //If tail pointer is not maintained
+/*      Node curr = head;
+        while (curr.next != head)
+            curr = curr.next;
+
+        curr.next = newNode;
+        newNode.next = head;
+        tail = newNode;
+ */
+    }
+
+    public void deleteFirst(){
+        if (isEmpty()) return;
+
+        if (head.next == head){
+            head = tail = null;
+            return;
+        }
+
+        head = head.next;
+        tail.next = head;
+
+        //If tail pointer is not maintained
+/*      Node curr = head;
+        while (curr.next != head)
+            curr = curr.next;
+
+        curr.next = head.next;
+        head = head.next;
+ */
+    }
+
+    public void deleteKthNode(int pos){
+        if (pos == 1) {
+            deleteFirst();
+            return;
+        }
+
+        Node curr = head;
+        for (int i = 0; i < pos - 2; i++)
+            curr = curr.next;
+
+        curr.next = curr.next.next;
+
+        //change tail pointer
+        if (curr.next == head)
+            tail = curr;
+    }
+
+    private boolean isEmpty(){
+        return head == null;
     }
 }
